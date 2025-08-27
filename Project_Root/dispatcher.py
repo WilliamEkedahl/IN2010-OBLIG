65dc50f3a877eee7d95f622d3d6d428284b3d939
Olavlel <olav.ellingsen@gmail.com>
2025-08-27 16:02:29 +0200

    test

---
+++ Project_Root/dispatcher.py
@@ -0,0 +1,9 @@
+
+from MySet import insert, remove, contains, size
+
+dispatcher = {
+        "insert": insert,
+        "remove": remove,
+        "contains": contains,
+        "size": size,
+    }
