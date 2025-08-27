b3fbb26d952b85e32724e25c88bb60bc400706bf
Olavlel <olav.ellingsen@gmail.com>
2025-08-27 16:38:49 +0200

    message

---
+++ TEST/binary_search_tree.java
@@ -0,0 +1,67 @@
+import java.util.Iterator;
+
+class binary_search_tree {
+
+
+public static void main(String[] args){
+
+    // Scanner-class 
+
+    // Les inn kommandoer
+
+    // Deretter utfør metoder
+
+    class Node {
+        Node left, right = null;
+        int data;
+
+        public Node (int x) { data = x; }
+
+    }
+
+    public boolean sjekkInnhold(int Tall) {
+
+        // let gjennom 
+
+        // hvis finner
+            return true;
+            
+        // else return false
+    }  
+
+
+
+    public void settInnNyttElement(int nyttTall) {
+
+        // Binært tre, dvs. left-right over eller under verdi
+        // Preorder algoritme for å gå igjennom treet.
+
+        // Sett inn element, oppdater peker se forelesning for eksempel
+        
+        // obs endres ved AVL
+        return; 
+    }  
+
+    public void slettElement(n) {
+
+        // Binært tre, dvs. left-right over eller under verdi
+        // Bruk postorder for å gå igjennom treet og slett baklengs
+        
+
+
+    }  
+
+    public int antall(n) {
+
+        // Binært tre, dvs. left-right over eller under verdi
+        // Bruk preorder for å gå igjennom treet og returner en integer som antall.
+
+        // 
+        
+
+    }      
+
+
+ }
+
+}
